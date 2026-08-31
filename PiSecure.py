d3177f831948e2c4a52174ffffb8f27c55c06041
Quin F. <cheesits456@haileybot.com>
2026-08-31 09:18:32 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -1,10 +1,10 @@
 import subprocess
 import time
 
-for i in range(60):
+for i in range(10*60):  # 1 minute of footage, 100ms * 10 * 60 
     subprocess.run(
         f"rpicam-jpeg -n -t 1 -o ./framebuffer/{i+1}.jpeg --hflip --vflip",
         shell=True,
         executable="/bin/bash",
     )
-    time.sleep(1)
+    time.sleep(0.1)
