e2023abf88c001990a1f8f228265db39fa6560fa
Quin F. <cheesits456@haileybot.com>
2026-08-31 08:24:38 -0700

    Try more parameters

--- PiSecure.py
+++ PiSecure.py
@@ -3,7 +3,18 @@ from libcamera import Transform
 
 picam2 = Picamera2()
 
-config = picam2.create_video_configuration({"size": (1920, 1080)}, transform=Transform(hflip=True, vflip=True))
+config = picam2.create_video_configuration(
+    {
+        "bit_depth": 8,
+        "crop_limits": (680, 692, 1920, 1080),
+        "exposure_limits": (75, 11766829, 20000),
+        "format": SRGGB8,
+        "fps": 47.57,
+        "size": (1920, 1080),
+        "unpacked": "SRGGB8",
+    },
+    transform=Transform(hflip=True, vflip=True),
+)
 picam2.configure(config)
 
 picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
