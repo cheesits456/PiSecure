d7ea2ad353cba9939881ffbc71544e19109efe98
Quin F. <quin@cheesits456.dev>
2026-08-31 14:11:52 -0700

    Add timestamp adding function

--- PiSecure.py
+++ PiSecure.py
@@ -2,6 +2,21 @@ import os
 import subprocess
 import time
 
+from PIL import Image, ImageDraw, ImageFont
+
+def add_timestamp_to_image(path):
+    print(f"Adding timestamp to {path}")
+
+    fontFile = "./FiraCodeMono.ttf"
+    fontSize = 65
+
+    img = Image.open(path)
+    I1 = ImageDraw.Draw(img)
+    font = ImageFont.truetype(fontFile, fontSize)
+
+    I1.text((10, 10), "2026-08-31 2:02PM", font=font, fill=(255, 255, 255))
+    img.save(path)
+
 # Clear old frames first
 print("Clearing old frames. . .")
 for file in os.listdir("./framebuffer"):
@@ -21,5 +36,6 @@ for i in range(60):
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
+    add_timestamp_to_image(savePath)
     print(f"Frame {frameNumber} generated")
     time.sleep(1)
