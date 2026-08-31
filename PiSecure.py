5cbe7c5917b6eabb41e6e13f54415ec4bb3f81d4
Quin F. <quin@cheesits456.dev>
2026-08-31 15:20:13 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -4,44 +4,56 @@ import time
 
 from PIL import Image, ImageDraw, ImageFont
 
+
 def add_timestamp_to_image(path):
-	print(f"Adding timestamp to {path}")
+    print(f"Adding timestamp to {path}")
 
-	fontFile = "./FiraCodeMono.ttf"
-	fontSize = 100
-	xPosition = 50
-	yPosition = 2320
+    fontFile = "./FiraCodeMono.ttf"
+    fontSize = 100
+    xPosition = 50
+    yPosition = 2320
 
-	img = Image.open(path)
-	image = ImageDraw.Draw(img)
-	font = ImageFont.truetype(fontFile, fontSize)
+    img = Image.open(path)
+    image = ImageDraw.Draw(img)
+    font = ImageFont.truetype(fontFile, fontSize)
 
-	image.rounded_rectangle([(0, 2290), (1150, 2464)], 25, "#0000007F", corners=("top_right"))
-	image.text((xPosition, yPosition), "2026-08-31 2:02PM", font=font, fill=(255, 255, 255))
-	img.save(path)
+    image.rounded_rectangle(
+        corners=(False, True, False, False),
+        fill=(0, 0, 0, 127),
+        radius=50,
+        xy=[(0, 2290), (1150, 2464)],
+    )
+    image.text(
+        fill=(255, 255, 255),
+        font=font,
+        text="2026-08-31 2:02PM",
+        xy=(xPosition, yPosition)
+    )
+    img.save(path)
+
 
 # Clear old frames first
 print("Clearing old frames. . .")
 for file in os.listdir("./framebuffer"):
-	os.remove(f"./framebuffer/{file}")
+    os.remove(f"./framebuffer/{file}")
 print("Done!")
 
 # Generate new frames for 1 minute
 print("Generating new frames. . .")
 for i in range(3):
-	frameNumber = i + 1
-	savePath = f"./framebuffer/{frameNumber}.jpeg"
-	subprocess.run(
-		f"rpicam-jpeg -n -o {savePath} -q 20 -t 1ms --hflip --vflip &",
-		executable="/bin/bash",
-		shell=True,
-		stdout=subprocess.DEVNULL,
-		stderr=subprocess.DEVNULL
-	)
-	print(f"Frame {frameNumber} generated")
-	time.sleep(1)
-	
+    frameNumber = i + 1
+    savePath = f"./framebuffer/{frameNumber}.jpeg"
+    subprocess.run(
+        args=f"rpicam-jpeg -n -o {savePath} -q 20 -t 1ms --hflip --vflip &",
+        executable="/bin/bash",
+        shell=True,
+        stderr=subprocess.DEVNULL,
+        stdout=subprocess.DEVNULL,
+    )
+    print(f"Frame {frameNumber} generated")
+    time.sleep(1)
+
 time.sleep(1)
 
 for file in os.listdir("./framebuffer"):
-	add_timestamp_to_image(f"./framebuffer/{file}")
+    add_timestamp_to_image(f"./framebuffer/{file}")
