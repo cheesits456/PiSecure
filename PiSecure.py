919b1fbd4d216f9d8564bfcca4066cad5605d90d
Quin F. <cheesits456@haileybot.com>
2026-08-30 19:42:35 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -1,6 +1,5 @@
 from picamera2 import Picamera2
-import time
 
 picam = Picamera2()
 
-picam.start_and_record_video("test_video.mp4", duration=5)
+picam.start_and_record_video("test_video.mp4", duration = 60 * 60)
