ed9b45b1a07cda6106c87fccba4d826c18f40601
Quin F. <cheesits456@haileybot.com>
2026-08-30 19:40:08 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -3,9 +3,4 @@ import time
 
 picam = Picamera2()
 
-camera_config = picam.create_preview_configuration()
-picam.configure(camera_config)
-
-picam.start()
-
 picam.start_and_record_video("test_video.mp4", duration=5)
