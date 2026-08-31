3c533db266c10a90926c069116cd3e4505fcb0a5
Quin F. <cheesits456@haileybot.com>
2026-08-31 08:16:19 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -1,5 +1,9 @@
 from picamera2 import Picamera2
+from libcamera import Transform
 
 picam2 = Picamera2()
 
-picam2.start_and_record_video("test_video.mkv", duration=60, hflip=True)  # duration in seconds
+config = picam2.create_video_configuration(transform=Transform(hflip=True))
+picam2.configure(config)
+
+picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
