a562af0638380aa1fdbeb60e465528be057e11fb
Quin F. <cheesits456@haileybot.com>
2026-08-31 10:33:36 -0700

    Put delay back to 1s

--- PiSecure.py
+++ PiSecure.py
@@ -8,10 +8,10 @@ for file in os.listdir("./framebuffer"):
     os.remove(f"./framebuffer/{file}")
 
 # Generate new frames for 1 minute
-for i in range(int(60.0 / 0.9)):
+for i in range(60):
     subprocess.run(
         f"rpicam-jpeg -n -o ./framebuffer/{i + 1}.jpeg -q 20 -t 1ms --hflip --vflip &",
         shell=True,
         executable="/bin/bash",
     )
-    time.sleep(0.899)
+    time.sleep(1)
