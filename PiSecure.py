ea7d04fd45f44392f109783daae6170e88deefc2
Quin F. <cheesits456@haileybot.com>
2026-08-31 08:21:30 -0700

    Test higher resolution

--- PiSecure.py
+++ PiSecure.py
@@ -3,7 +3,7 @@ from libcamera import Transform
 
 picam2 = Picamera2()
 
-config = picam2.create_video_configuration(transform=Transform(hflip=True, vflip=True))
+config = picam2.create_video_configuration({"size": (1920, 1080)}, transform=Transform(hflip=True, vflip=True))
 picam2.configure(config)
 
 picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
