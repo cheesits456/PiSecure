76bbdda1ffa91d9c1b7d41848833621f47eb7096
Quin F. <cheesits456@haileybot.com>
2026-08-31 06:31:31 -0700

    Bug fix config test

--- PiSecure.py
+++ PiSecure.py
@@ -1,11 +1,14 @@
 from pprint import *
 from picamera2 import Picamera2
 
-picam = Picamera2()
-config = {"format": "SRGGB8", "size": (3280, 2464)}
+picam2 = Picamera2()
 
-picam.configure(config)
+config = picam2.create_preview_configuration({"format": "SRGGB8", "size": (3280, 2464)})
+picam2.align_configuration(config)
+config["main"]
+
+picam2.configure(config)
 
 # pprint(picam.sensor_modes)
 
-picam.start_and_record_video("test_video.mp4", duration=60)
+picam2.start_and_record_video("test_video.mp4", duration=60)
