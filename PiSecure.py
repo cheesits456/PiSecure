5c26e31085a4697cfe122532d5814feb53772865
Quin F. <cheesits456@haileybot.com>
2026-08-31 08:07:08 -0700

    Oh fuck go back

--- PiSecure.py
+++ PiSecure.py
@@ -1,11 +1,5 @@
-# from picamera2 import Picamera2
+from picamera2 import Picamera2
 
-# picam2 = Picamera2()
+picam2 = Picamera2()
 
-# picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
-
-from picamera import PiCamera
-
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.capture('test_image.jpg')
+picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
