a6a4a7289ec58a36368f3e76a8b5047cad6d3d99
Quin F. <cheesits456@haileybot.com>
2026-08-31 06:41:21 -0700

    Give up on configs

--- PiSecure.py
+++ PiSecure.py
@@ -3,11 +3,6 @@ from picamera2 import Picamera2
 
 picam2 = Picamera2()
 
-config = picam2.create_preview_configuration({"format": "BGR888", "size": (3280, 2464)})
-picam2.align_configuration(config)
-
-picam2.configure(config)
-
 # pprint(picam.sensor_modes)
 
-picam2.start_and_record_video("test_video.mp4", duration=60)
+picam2.start_and_record_video("test_video.mkv", duration=60)
