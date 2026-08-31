5cfde4d89da8052dbce9c1586207c411f9127ce6
Quin F. <cheesits456@haileybot.com>
2026-08-31 09:13:57 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -1,14 +1,10 @@
-# from picamera2 import Picamera2
-# from libcamera import Transform
-
-# picam2 = Picamera2()
-
-# config = picam2.create_video_configuration({"size": (3280, 2464)}, transform=Transform(hflip=True, vflip=True))
-# picam2.configure(config)
-# picam2.video_configuration.controls.FrameRate = 10.0
-
-# picam2.start_and_record_video("test_video.mkv", duration=60)  # duration in seconds
-
 import subprocess
+import time
 
-subprocess.run("rpicam-jpeg -n -t 1 -o test_image.jpeg --hflip --vflip", shell = True, executable="/bin/bash")
+for i in range(60):
+    subprocess.run(
+        f"rpicam-jpeg -n -t 1 -o {i+1}.jpeg --hflip --vflip",
+        shell=True,
+        executable="/bin/bash",
+    )
+    time.sleep(1)
