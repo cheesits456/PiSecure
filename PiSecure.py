3dcedce1a89ed0c13f36281cc84943c52921cbaa
Quin F. <cheesits456@haileybot.com>
2026-08-31 10:29:42 -0700

    Adjust delay between frames to 0.75s

--- PiSecure.py
+++ PiSecure.py
@@ -8,10 +8,10 @@ for file in os.listdir("./framebuffer"):
     os.remove(f"./framebuffer/{file}")
 
 # Generate new frames for 1 minute
-for i in range(1*60):
+for i in range(60 / 0.75):
     subprocess.run(
-        f"rpicam-jpeg -n -o ./framebuffer/{i+1}.jpeg -q 20 -t 1ms --hflip --vflip &",
+        f"rpicam-jpeg -n -o ./framebuffer/{i + 1}.jpeg -q 20 -t 1ms --hflip --vflip &",
         shell=True,
         executable="/bin/bash",
     )
-    time.sleep(0.999)
+    time.sleep(0.749)
