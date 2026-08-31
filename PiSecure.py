08e0cc57720744224559221fc5f250f81991e617
Quin F. <cheesits456@haileybot.com>
2026-08-31 07:48:41 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -4,9 +4,6 @@ import time
 
 picam2 = Picamera2()
 
-video_config = picam2.create_video_configuration()
-picam2.configure(video_config)
-
 output = FfmpegOutput("test_video.mkv")
 
 picam2.start_recording(output)
