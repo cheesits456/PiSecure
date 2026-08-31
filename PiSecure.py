26c16d3ba12c88c9479320cbdac4d6e8ac737e62
Quin F. <cheesits456@haileybot.com>
2026-08-31 09:14:44 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -3,7 +3,7 @@ import time
 
 for i in range(60):
     subprocess.run(
-        f"rpicam-jpeg -n -t 1 -o {i+1}.jpeg --hflip --vflip",
+        f"rpicam-jpeg -n -t 1 -o ./framebuffer/{i+1}.jpeg --hflip --vflip",
         shell=True,
         executable="/bin/bash",
     )
