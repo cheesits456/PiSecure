f4f41b64f48006dde805aaf03b826025a4f53b08
Quin F. <cheesits456@haileybot.com>
2026-08-31 10:08:08 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -1,16 +1,16 @@
 import os
 import subprocess
+import threading
 import time
 
 # Clear old frames first
-frames = os.listdir("./framebuffer")
-for file in frames:
+for file in os.listdir("./framebuffer"):
     os.remove(f"./framebuffer/{file}")
 
 # Generate new frames for 1 minute
 for i in range(10*60):  # 100ms * 10 * 60
     subprocess.run(
-        f"rpicam-jpeg -n -t 100ms -o ./framebuffer/{i+1}.jpeg --hflip --vflip",
+        f"rpicam-jpeg -n -o ./framebuffer/{i+1}.jpeg -q 20 -t 100ms --hflip --vflip &",
         shell=True,
         executable="/bin/bash",
     )
