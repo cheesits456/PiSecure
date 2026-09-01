2855107aa6514713bfdbb7a0b4de3fc404e43cc1
Quin F. <quin@cheesits456.dev>
2026-09-01 08:52:55 -0700

    Update PiSecure.py

--- PiSecure.py
+++ PiSecure.py
@@ -81,6 +81,7 @@ with ThreadPoolExecutor(max_workers=batchSize) as executor:
     
         for file in os.listdir("./framebuffer"):
             frameNumber = int(file.split(".")[0])
+            print(frameNumber)
             if frameNumber > frameBatches * batchSize and frameNumber <= (frameBatches + 1) * batchSize:
                 executor.submit(add_timestamp_to_image, f"./framebuffer/{file}")
     
