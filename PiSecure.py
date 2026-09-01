938f5df5cfe87563cf4dde79809225cc8f92ef94
Quin F. <quin@cheesits456.dev>
2026-09-01 07:44:25 -0700

    Remove decimal values

--- PiSecure.py
+++ PiSecure.py
@@ -15,7 +15,7 @@ def add_timestamp_to_image(path):
     yPosition = 2320
     
     timestamp = os.path.getmtime(path)
-    print(datetime.fromtimestamp(timestamp))
+    print(datetime.fromtimestamp(timestamp).split(".")[0])
 
     img = Image.open(path).convert("RGBA")
     font = ImageFont.truetype(fontFile, fontSize)
