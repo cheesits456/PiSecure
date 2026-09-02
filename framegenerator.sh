79fcd9cef9c78d6f7986964e912976918c933ea3
Quin F. <quin@cheesits456.dev>
2026-09-02 06:21:29 -0700

    Update framegenerator.sh

--- framegenerator.sh
+++ framegenerator.sh
@@ -1,6 +1,9 @@
 #!/bin/env bash
 
-for i in {0..10}; do
+# Clear framebuffer first
+rm "./framebuffer/*"
+
+for i in {1..11}; do # Loop through numbers 0 - 10
 	rpicam-jpeg -n -o ./framebuffer/"$i".jpeg -q 20 -t 1ms --hflip --vflip 2>/dev/null &
 	sleep 1
 done
