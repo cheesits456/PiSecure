9d5568a5496883a4531b3b7f7309f5c8349b9864
Quin F. <quin@cheesits456.dev>
2026-09-03 07:51:51 -0700

    Update framegenerator.sh

--- framegenerator.sh
+++ framegenerator.sh
@@ -7,7 +7,7 @@ echo "Generating new frames. . ."
 # Loop while file ./stopFrameGenerator doesn't exist
 while [ ! -f "./stopFrameGenerator" ]; do
 	(( frameNumber += 1 ))
-	rpicam-jpeg -n -o "./framebuffer/$frameNumber.jpeg" -q 20 -t 1ms --hflip --vflip 2>/dev/null &
+	rpicam-jpeg -n -o "./framebuffer/$frameNumber.jpeg" -q 20 -t 1ms --hflip --vflip &
 	echo "Frame $frameNumber generated!"
 	sleep 1
 done
