f7d6cad0f984567a03172085ee43ef3e11af5280
Quin F. <quin@cheesits456.dev>
2026-09-03 09:17:47 -0700

    Update classes.py

--- helpers/classes.py
+++ helpers/classes.py
@@ -1,3 +1,6 @@
+import threading
+
+from concurrent.futures import ThreadPoolExecutor
 from watchdog.events import FileSystemEventHandler
 
 from helpers.functions import add_timestamp_to_image
@@ -5,4 +8,5 @@ from helpers.functions import add_timestamp_to_image
 
 class FileCreateHandler(FileSystemEventHandler):
     def on_created(self, event):
-        add_timestamp_to_image(event.src_path)
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            executor.submit(add_timestamp_to_image, event.src_path)
