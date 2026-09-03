e5309d3e050927f63d201c5a21ca95a43047efa5
Quin F. <quin@cheesits456.dev>
2026-09-03 09:23:56 -0700

    Update classes.py

--- helpers/classes.py
+++ helpers/classes.py
@@ -7,4 +7,7 @@ from helpers.functions import add_timestamp_to_image
 class FileCreateHandler(FileSystemEventHandler):
     def on_created(self, event):
         with ThreadPoolExecutor(max_workers=4) as executor:
-            executor.submit(add_timestamp_to_image, event.src_path)
+            add_timestamp_to_image(event.src_path)
+            if not int(event.src_path.split("/")[1].split(".")[0]) % 10:
+                executor.submit(print, "10 images captured!")
+            
