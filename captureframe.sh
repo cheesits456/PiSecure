b42f7bcb6f9dcec55b8b9040fdee6d9acbccf13c
Quin F. <quin@cheesits456.dev>
2026-09-03 08:24:21 -0700

    Make new sh file only for capturing a single frame

---
+++ captureframe.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+frameNumber="$1"
+rpicam-jpeg -n -o "./framebuffer/$frameNumber.jpeg" -q 20 -t 1ms --hflip --vflip 2>/dev/null && echo "Frame $frameNumber captured!"
--- framegenerator.sh
+++ framegenerator.sh
@@ -7,8 +7,7 @@ echo "Generating new frames. . ."
 # Loop while file ./stopFrameGenerator doesn't exist
 while [ ! -f "./stopFrameGenerator" ]; do
 	(( frameNumber += 1 ))
-	rpicam-jpeg -n -o "./framebuffer/$frameNumber.jpeg" -q 20 -t 1ms --hflip --vflip 2>/dev/null &
-	echo "Frame $frameNumber generated!"
+	./captureframe $frameNumber &
 	sleep 1
 done
 rm "./stopFrameGenerator"
