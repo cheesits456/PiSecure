6d39f4e9cd3ddce098a03e720b9a2dc22a2066fb
Quin F. <quin@cheesits456.dev>
2026-09-03 08:53:27 -0700

    Update framegenerator.sh

--- helpers/framegenerator.sh
+++ helpers/framegenerator.sh
@@ -7,7 +7,7 @@ echo "Generating new frames. . ."
 # Loop while file ./stopFrameGenerator doesn't exist
 while [ ! -f "./stopFrameGenerator" ]; do
 	(( frameNumber += 1 ))
-	./captureframe.sh $frameNumber &
+	./helpers/captureframe.sh $frameNumber &
 	sleep 1
 done
 rm "./stopFrameGenerator"
