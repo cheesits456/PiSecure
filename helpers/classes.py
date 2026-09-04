d47fabb9d80c1face5804574e2e27bef83a0ae42
Quin F. <quin@cheesits456.dev>
2026-09-03 19:43:26 -0700

    Call new function

--- helpers/classes.py
+++ helpers/classes.py
@@ -1,7 +1,7 @@
 from concurrent.futures import ThreadPoolExecutor
 from watchdog.events import FileSystemEventHandler
 
-from helpers.functions import add_timestamp_to_image
+from helpers.functions import add_timestamp_to_image, convert_frames_to_video
 
 
 class FileCreateHandler(FileSystemEventHandler):
@@ -13,5 +13,5 @@ class FileCreateHandler(FileSystemEventHandler):
             # The variable event.src_path == './framebuffer/n.jpeg', where 'n' is the frame number
             frameNumber = int(event.src_path.split("/")[2].split(".")[0])
             if frameNumber % 10 == 0:
-                executor.submit(print, "10 images captured!")
+                executor.submit(convert_frames_to_video, frameNumber)
                 
