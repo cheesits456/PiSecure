8017ed997077bd0080c08004927a0f5c60a7f853
Quin F. <quin@cheesits456.dev>
2026-09-04 08:48:52 -0700

    Add new log messages

--- helpers/framegenerator.sh
+++ helpers/framegenerator.sh
@@ -8,7 +8,9 @@ frameNumber=0
 # Loop while file ./stopFrameGenerator doesn't exist
 while [ ! -f "./stopFrameGenerator" ]; do
 	(( frameNumber += 1 ))
+	[[ $debugLevel -ge 3 ]] && echo "Capturing frame ${frameNumber}. . ."
 	./helpers/captureframe.sh $frameNumber &
+	[[ $debugLevel -ge 2 ]] && echo "Frame ${frameNumber} captured!"
 	sleep 1
 done
 rm "./stopFrameGenerator"
