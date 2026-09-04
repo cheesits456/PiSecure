6203eba88678551c46e77df451893c2fee6b53e9
Quin F. <quin@cheesits456.dev>
2026-09-04 10:12:20 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # How many frames to use per video file, 1 frame = 1 second
-batchSize=60
+batchSize=20
 
 # Adjust how many debug messages get printed, higher number = more messages, set to 0 to silence most logs
 frameCaptureDebugLevel=2
