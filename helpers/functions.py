f5459ede7a7e5831c0fb393ccee76d6980536b9c
Quin F. <quin@cheesits456.dev>
2026-09-05 03:37:36 -0700

    Update functions.py

--- helpers/functions.py
+++ helpers/functions.py
@@ -68,7 +68,7 @@ def convert_frames_to_video(frameCount: int, batchSize=batchSize):
     if videoGenerationDebugLevel >= 2: print(f"Generating video file for files {frames[0]} through {frames[-1]}. . .")
 
     frame = cv2.imread(frames[0])
-    height, width = frame.shape
+    height, width, layers = frame.shape
     timestamp = str(datetime.fromtimestamp(os.path.getctime(frames[0]))).split(".")[0]
     foldername, filename = timestamp.split(" ")
     outputFolder = f"./footage/{foldername}"
