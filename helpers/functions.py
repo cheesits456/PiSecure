c979b5d0e9bf9f7d37ffbeba1cdbf7c02e2f1247
Quin F. <quin@cheesits456.dev>
2026-09-06 09:28:03 -0700

    Update functions.py

--- helpers/functions.py
+++ helpers/functions.py
@@ -110,10 +110,10 @@ def sort_frame_list_by_number(filename: str):
 
 
 def split_message(text: str, maxLength=2000, char='\n', prepend="", append=""):
-    if len(text) <= maxLength: return text
+    if len(text) + len(prepend) + len(append) <= maxLength: return [prepend + text + append]
     splitText = text.split(char)
     for chunk in splitText:
-        if len(chunk) > maxLength: return "Message exceeds the max length and / or contains no or not enough split characters"
+        if len(chunk) > maxLength: return ["Message exceeds the max length and / or contains no or not enough split characters"]
     messages = [""];
     messageNumber = 0;
     for i in range(0, len(splitText)):
