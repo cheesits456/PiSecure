60ca77d7bc5963db895284b30bc9a4866bc0a971
Quin F. <quin@cheesits456.dev>
2026-09-08 19:57:11 -0700

    Update botInterface.py

--- botInterface.py
+++ botInterface.py
@@ -39,7 +39,7 @@ async def on_ready() -> None:
 ])
 @app_commands.describe(folder="Which folder to list the contents of")
 async def list(interaction: discord.Interaction, folder: str) -> None:
-    if interaction.user.id == userID: return await interaction.response.send_message("Invalid user")
+    if interaction.user.id != userID: return await interaction.response.send_message("Invalid user")
     command = f"ls {folder}" if folder == "framebuffer" else f"tree {folder}"
     res = subprocess.run(
         args = command,
