1f274ca7ce3529644faa818cf3b3715eccd6af7a
Quin F. <quin@cheesits456.dev>
2026-09-10 05:29:07 -0700

    Update botInterface.py

--- botInterface.py
+++ botInterface.py
@@ -105,7 +105,7 @@ async def shell(interaction: discord.Interaction, input: str) -> None:
             await interaction.channel.send(f"```{splitResult[i]}```")
     
     with ThreadPoolExecutor(max_workers=1) as executor:
-        executor.submit(await run_command_and_send_output_to_channel)
+        await executor.submit(run_command_and_send_output_to_channel)
 
 
 
